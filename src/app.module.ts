877be15712d0bb8e050d05f1f5a07c09064f88d9
Donadams50 <sumbomatic@gmail.com>
2024-04-18 11:07:12 +0100

    fix  docker file path issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,26 +6,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { InferenceOrchestratorEntity } from './InferenceOrchestrator/sqlite/inference-orchestrator.entity';
 import * as path from 'path';
 
-
-import * as dotenv from 'dotenv';
-
-dotenv.config(); 
-const SQLITE_FOLDER_NAME = '/sqliteStorage';
-const SQLITE_FILE_NAME = 'database.sqlite';
-const sqliteFileName = path.join(SQLITE_FOLDER_NAME, SQLITE_FILE_NAME);
-
 @Module({
   imports: [
     InferenceOrchestratorModule,
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'sqlite',
-        database: sqliteFileName, 
-        entities: [InferenceOrchestratorEntity], 
+        database: path.join(__dirname, '..', 'sqliteStorage', 'database.sqlite'),
+        entities: [InferenceOrchestratorEntity],
         synchronize: true,
         logging: true
       }),
-      inject: [],
     }),
   ],
   controllers: [AppController],
